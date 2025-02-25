d4767b65cd2234e46423d78a8931b09595b19d0e
hmani3 <heemanijaz@gmail.com>
2025-02-25 10:26:44 -0500

    add color picker

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -1,5 +1,27 @@
 window.addEventListener('load', function () {
-    let c = this.document.getElementById('my-canvas');
+    const c = this.document.getElementById('my-canvas');
+    const cPicker = this.document.getElementById('color-picker')
     let ctx = c.getContext('2d');
-    
+    let curColor = "#000000";
+    let objs = [];
+    cPicker.addEventListener('change', function() {
+        curColor = cPicker.value;
+    });
+
+    // Change color
+    c.addEventListener('click',function(e){
+        // Convert hex color (#RRGGBB) to rgb string
+        
+        const rect = c.getBoundingClientRect();
+        const x = e.clientX - rect.left;
+        const y = e.clientY - rect.top;
+        const bigint = parseInt(curColor.slice(1), 16);
+        const r = (bigint >> 16) & 255;
+        const g = (bigint >> 8) & 255;
+        const b = bigint & 255;
+        ctx.fillStyle = `rgb(${r}, ${g}, ${b})`;
+        
+        ctx.fillRect(x, y, 50,50);
+    })
+
 });
