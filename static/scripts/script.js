fd5f160237ddec0889a47d58bb54ed53e0454c67
hmani3 <heemanijaz@gmail.com>
2025-03-04 09:56:32 -0500

    completely implemenet drawing rectangles of chosen size, with preview

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -4,7 +4,9 @@ window.addEventListener('load', function () {
     let ctx = c.getContext('2d');
     let curColor = cPicker.value;
     let objs = [];
-
+    let mode = 'none';
+    let isDrawing = false;
+    let startX, startY, previewX, previewY;
     class Rectangle {
         constructor(x = 0, y = 0, width = 50, height = 50, color = curColor) {
             this.x = x;
@@ -22,69 +24,118 @@ window.addEventListener('load', function () {
             ctx.fillRect(this.x, this.y, this.width, this.height);
         }
     }
-    class Triangle {
-        constructor(x = 0, y = 0, width = 50, height = 50, color = curColor) {
-            this.x = x;
-            this.y = y;
-            this.width = width;
-            this.height = height;
-            this.color = color;
-        }
-        draw() {
-            this.bigint = parseInt(this.color.slice(1), 16);
-            this.r = (this.bigint >> 16) & 255;
-            this.g = (this.bigint >> 8) & 255;
-            this.b = this.bigint & 255;
-            ctx.fillStyle = `rgb(${this.r}, ${this.g}, ${this.b})`;
-            ctx.fillRect(this.x, this.y, this.width, this.height);
-        }
-    }
-    class Circle {
-        constructor(x = 0, y = 0, width = 50, height = 50, color = curColor) {
-            this.x = x;
-            this.y = y;
-            this.width = width;
-            this.height = height;
-            this.color = color;
-        }
-        draw() {
-            this.bigint = parseInt(this.color.slice(1), 16);
-            this.r = (this.bigint >> 16) & 255;
-            this.g = (this.bigint >> 8) & 255;
-            this.b = this.bigint & 255;
-            ctx.fillStyle = `rgb(${this.r}, ${this.g}, ${this.b})`;
-            ctx.fillRect(this.x, this.y, this.width, this.height);
-        }
-    }
+    // class Triangle {
+    //     constructor(x = 0, y = 0, width = 50, height = 50, color = curColor) {
+    //         this.x = x;
+    //         this.y = y;
+    //         this.width = width;
+    //         this.height = height;
+    //         this.color = color;
+    //     }
+    //     draw() {
+    //         this.bigint = parseInt(this.color.slice(1), 16);
+    //         this.r = (this.bigint >> 16) & 255;
+    //         this.g = (this.bigint >> 8) & 255;
+    //         this.b = this.bigint & 255;
+    //         ctx.fillStyle = `rgb(${this.r}, ${this.g}, ${this.b})`;
+    //         ctx.fillRect(this.x, this.y, this.width, this.height);
+    //     }
+    // }
+    // class Circle {
+    //     constructor(x = 0, y = 0, width = 50, height = 50, color = curColor) {
+    //         this.x = x;
+    //         this.y = y;
+    //         this.width = width;
+    //         this.height = height;
+    //         this.bigint = parseInt(color.slice(1), 16);
+    //         this.r = (this.bigint >> 16) & 255;
+    //         this.g = (this.bigint >> 8) & 255;
+    //         this.b = this.bigint & 255;
+    //         this.color = `rgb(${this.r}, ${this.g}, ${this.b})`;
+    //     }
+    //     draw() {
+    //         ctx.fillStyle = color;
+    //         ctx.fillRect(this.x, this.y, this.width, this.height);
+    //     }
+    //     drawPreview(){
+    //         ctx.setLineDash([5, 5]); // Dashed line for preview
+    //         ctx.strokeStyle = "black"
+    //         ctx.fillRect(this.x, this.y, this.width, this.height)
+    //     }
+    // }
 
 
     cPicker.addEventListener('change', function () {
         curColor = cPicker.value;
     });
 
-    // Change color
-    c.addEventListener('click', function (e) {
-        // Convert hex color (#RRGGBB) to rgb string
-        const rect = c.getBoundingClientRect();
-        let x = e.clientX - rect.left;
-        let y = e.clientY - rect.top;
-        let square = new Rectangle(x, y, 100, 100, curColor);
-        square.draw();
-        objs.push(square);
-        console.log(objs);
+    c.addEventListener('mousedown', function (e) {
+        if (mode === 'rect') {
+            if (!isDrawing) {
+                isDrawing = true;
+                startX = e.offsetX;
+                startY = e.offsetY;
+
+            } else {
+                isDrawing = false;
+                let curRect = new Rectangle(startX, startY, previewX - startX, previewY - startY, curColor);
+                curRect.draw();
+                objs.push(curRect);
+            }
+
+        } else if (mode === 'tri') {
+        } else if (mode === 'circ') {
+        } else {
+
+        }
+    });
+
+    c.addEventListener('mousemove', function (e) {
+        if (mode === 'rect') {
+            if (isDrawing) {
+                previewX = e.offsetX;
+                previewY = e.offsetY;
+                let curRect = new Rectangle(startX, startY, previewX - startX, previewY - startY, curColor);
+                ctx.clearRect(0, 0, c.width, c.height);
+                objs.forEach(element => {
+                    element.draw();
+                });
+                curRect.draw();
+            }
+        
+        } else if (mode === 'tri') {
+        } else if (mode === 'circ') {
+        } else {
+
+        }
     })
-    document.getElementById('undo-button')
-    .addEventListener('click', function () {
-        objs.pop();
-        ctx.clearRect(0, 0, c.width, c.height);
-        objs.forEach(element => {
-            element.draw();
+
+    document.getElementById('rectangle-button')
+        .addEventListener('click', function () {
+            mode = 'rect';
         });
-    })
+    document.getElementById('triangle-button')
+        .addEventListener('click', function () {
+            mode = 'tri';
+        });
+    document.getElementById('circle-button')
+        .addEventListener('click', function () {
+            mode = 'circ';
+        });
+
+    document.getElementById('undo-button')
+        .addEventListener('click', function () {
+            objs.pop();
+            ctx.clearRect(0, 0, c.width, c.height);
+            objs.forEach(element => {
+                element.draw();
+            });
+        });
     document.getElementById('clear-button')
-    .addEventListener('click',function(){
-        ctx.clearRect(0, 0, c.width, c.height);
-        objs=[];
-    })
+        .addEventListener('click', function () {
+            ctx.clearRect(0, 0, c.width, c.height);
+            objs = [];
+            mode = 'none';
+        });
 
 });
